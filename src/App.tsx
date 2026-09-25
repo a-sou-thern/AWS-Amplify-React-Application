66f613e3f748df258f1054395f39417b026bf1aa
a-sou-thern <alex.southern@gmail.com>
2026-09-24 17:55:23 -0700

    Fixed Issue with Delete Function

--- src/App.tsx
+++ src/App.tsx
@@ -28,16 +28,16 @@ function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        Time to add a new To Do!
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
         </a>
-              <button onClick={signOut}>Sign out</button>
       </div>
       <button onClick={signOut}>Sign out</button>
     </main>
